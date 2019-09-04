ba0133f4a2628172086d34df96d6941ff141781d
imac <imac@Schneidler-iMac.local>
2019-09-04 20:54:37 +0200

    #3: Ami még kimaradt

---
+++ l2l.Data/Repository/CourseRepository.cs
@@ -0,0 +1,17 @@
+using l2l.Data.Model;
+
+namespace l2l.Data.Repository
+{
+    public class CourseRepository
+    {
+        public void Add(Course entity)
+        {
+           
+        }
+
+        public Course GetById(int Id)
+        {
+            return new Course();
+        }
+    }
+}
